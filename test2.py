d54750a9289ee5488ef3bea0b9db0f4476c78fc9
It's Kat! <leongk@berkeley.edu>
2026-02-04 15:44:40 -0800

    Add files via upload
    second test using Adafruit_bme680

---
+++ test2.py
@@ -0,0 +1,22 @@
+import time
+import board
+import digitalio
+import adafruit_bme680
+
+cs = digitalio.DigitalInOut(board.D10)
+spi = board.SPI()
+bme680 = adafruit_bme680.Adafruit_BME680_SPI(spi, cs)
+
+bme680.sea_level_pressure = 1013.25
+temperature_offset = -5
+
+i = 0
+while (i <= 5):
+	print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
+	print("Gas: %d ohm" % bme680.gas)
+	print("Humidity: %0.1f %%" % bme680.relative_humidity)
+	print("Pressure: %0.3f hPa" % bme680.pressure)
+	print("Altitude = %0.2f meters" % bme680.altitude)
+	i++
+	
+	time.sleep(1)
