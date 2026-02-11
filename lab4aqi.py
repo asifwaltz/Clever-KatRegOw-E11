ab2fa6331074ef3c4688a18437543785e30da451
reggie <88353429+IndigoPlatypus@users.noreply.github.com>
2026-02-11 15:37:18 -0800

    Update lab4aqi.py
    accidnenally deletd reset pin -added it in

--- lab4aqi.py
+++ lab4aqi.py
@@ -8,6 +8,7 @@ uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
 
 
 from adafruit_pm25.uart import PM25_UART
+reset_pin = None
 pm25 = PM25_UART(uart, reset_pin)
 
 print("Found PM2.5 sensor, reading data :3...")
