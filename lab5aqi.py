268a8e0d4014cf714e0d6152a6818073d4f86a24
reggie <88353429+IndigoPlatypus@users.noreply.github.com>
2026-02-18 15:27:15 -0800

    Update lab5aqi.py
    added arguments for file name + duration of runtime

--- lab5aqi.py
+++ lab5aqi.py
@@ -1,11 +1,19 @@
 import time
 import board
 import busio 
-import csv
-
 from digitalio import DigitalInOut, Direction, Pull
+import csv
 import numpy as np
 import sys
+
+args = sys.argv #want arguments for file name and duration
+data_path = "data/" + args[1]
+
+file = open(data_path, "w", newline = None)
+csvwrt = csv.writer(file, delimiter = ",")
+
+meta = ["time", "stndr10", "stndr25", "stndr100", "enviro10", "enviro25", "enviro100", "p03um", "p05um", "p10um", "p25um", "p50um", "p100um"]
+csvwrt.writerow(meta)
 
 import serial
 uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=0.25)
@@ -14,27 +22,20 @@ from adafruit_pm25.uart import PM25_UART
 reset_pin = None
 pm25 = PM25_UART(uart, reset_pin)
 
-print("Found PM2.5 sensor, reading data...")
-
-file = open("data/test.csv", "w", newline = None) # file initialization
-csvwrt = csv.writer(file, delimiter = ",")
-
-meta = ["time", "stndr10", "stndr25", "stndr100", "enviro10", "enviro25", "enviro100"]
-csvwrt.writerow(meta)
+print("Found PM2.5 sensor, reading data :3...")
 
 now = time.time()
-val = 0
-duration = 30 #seconds it runs for
+duration = args[2] #seconds it runs for
 while (time.time() < now + duration):
     time.sleep(1)
     
     try:
         aqdata = pm25.read()
     except RuntimeError:
-        print("Unable to read from sensor. Retrying...")
+        print("Unable to read from sensor you idiot. Retrying...")
         continue
 
-    csvwrt.writerow([time.ctime(), aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"], aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"]]) 
+    csvwriter.writerow([time.ctime(), aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"],aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"],aqdata["particles 03um"],aqdata["particles 05um"],aqdata["particles 10um"],aqdata["particles 25um"],aqdata["particles 50um"],aqdata["particles 100um"]])
     
     print()
     print("Concentration Units (standard)")
@@ -62,31 +63,5 @@ while (time.time() < now + duration):
 for i in range(duration):
     nownow = time.time()
     csvwrt.writerow([nownow, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"], aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"]]) #values not being recorded 
-    # can't you just use ctime()?
-    
-args = sys.argv
-print(args)
-
-# if len(sys.argv) != 3:
-#     print("Usage: python script.py <filename> <runtime>")
-#     sys.exit(1)
-
-# data_path = sys.argv[1]
-# runtime = int(sys.argv[2])
-
-data_path = "data/" + args[1]
-runtime = int(args[2])
-
-file = open(data_path, "w", newline = None)
-
-csvwriter = csv.writer(file, delimiter = ",")
-
-meta = ["time", "data"]
-csvwriter.writerow(meta)
-
-for i in range(runtime):
-  now = time.time()
-  value = np.random.random()
-  csvwriter.writerow([now, value])
 
 file.close()
