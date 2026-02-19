3e3b9f58fc5ad416622feb6861f8d848f56588bd
reggie <88353429+IndigoPlatypus@users.noreply.github.com>
2026-02-18 16:07:51 -0800

    Update lab5aqi.py
    man idk

--- lab5aqi.py
+++ lab5aqi.py
@@ -45,10 +45,6 @@ while (time.time() < now + runtime):
     print(f"\nTime: {time.ctime()}s")
     print()
     print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
-	'''Sprint("Gas: %d ohm" % bme680.gas)
-	print("Humidity: %0.1f %%" % bme680.relative_humidity)
-	print("Pressure: %0.3f hPa" % bme680.pressure)
-	print("Altitude = %0.2f meters" % bme680.altitude)'''
     print("Concentration Units (standard)")
     print("----------------------------------------")
     print(
