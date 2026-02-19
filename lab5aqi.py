7b4cc2a234062f9df476528da0f2e7e781c436d3
reggie <88353429+IndigoPlatypus@users.noreply.github.com>
2026-02-18 16:03:13 -0800

    Update lab5aqi.py
    should get weather data too

--- lab5aqi.py
+++ lab5aqi.py
@@ -5,6 +5,13 @@ from digitalio import DigitalInOut, Direction, Pull
 import csv
 import numpy as np
 import sys
+import adafruit_bme680
+
+
+i2c = board.I2C()
+bme680 = adafruit_bme680.Adafruit_BME_I2C(i2c, debug=False)
+bme680.sea_level_pressure = 1013.25
+temperature_offset = -5
 
 args = sys.argv
 dataPath = "data/" + args[1]
@@ -13,7 +20,7 @@ runtime = int(args[2])
 file = open(dataPath, "w", newline = None)
 csvwrt = csv.writer(file, delimiter = ",")
 
-meta = ["time", "stndr10", "stndr25", "stndr100", "enviro10", "enviro25", "enviro100", "p03um", "p05um", "p10um", "p25um", "p50um", "p100um"]
+meta = ["time", "temp", "gas", "humidity", "pressure", "alt", "stndr10", "stndr25", "stndr100", "enviro10", "enviro25", "enviro100", "p03um", "p05um", "p10um", "p25um", "p50um", "p100um"]
 csvwrt.writerow(meta)
 
 import serial
@@ -34,11 +41,14 @@ while (time.time() < now + runtime):
     except RuntimeError:
         print("Unable to read from sensor you idiot. Retrying...")
         continue
-
-    csvwrt.writerow([time.ctime(), aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"],aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"],aqdata["particles 03um"],aqdata["particles 05um"],aqdata["particles 10um"],aqdata["particles 25um"],aqdata["particles 50um"],aqdata["particles 100um"]])
     
     print(f"\nTime: {time.ctime()}s")
     print()
+    print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
+	print("Gas: %d ohm" % bme680.gas)
+	print("Humidity: %0.1f %%" % bme680.relative_humidity)
+	print("Pressure: %0.3f hPa" % bme680.pressure)
+	print("Altitude = %0.2f meters" % bme680.altitude)
     print("Concentration Units (standard)")
     print("----------------------------------------")
     print(
@@ -61,8 +71,10 @@ while (time.time() < now + runtime):
     print("Particles > 10 um / 0.1L air:", aqdata["particles 100um"])
     print("----------------------------------------")
 
+    csvwrt.writerow([nownow, bme680.temperature + temperature_offset, bme680.gas, bme680.relative_humidity, bme680.pressure, bme680.altitude, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"], aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"],aqdata["particles 03um"], aqdata["particles 05um"], aqdata["particles 10um"], aqdata["particles 25um"], aqdata["particles 50um"], aqdata["particles 100um"]])
+
 for i in range(runtime):
     nownow = time.time()
-    csvwrt.writerow([nownow, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"], aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"],aqdata["particles 03um"], aqdata["particles 05um"], aqdata["particles 10um"], aqdata["particles 25um"], aqdata["particles 50um"], aqdata["particles 100um"]])
+    csvwrt.writerow([nownow, bme680.temperature + temperature_offset, bme680.gas, bme680.relative_humidity, bme680.pressure, bme680.altitude, aqdata["pm10 standard"], aqdata["pm25 standard"], aqdata["pm100 standard"], aqdata["pm10 env"], aqdata["pm25 env"], aqdata["pm100 env"],aqdata["particles 03um"], aqdata["particles 05um"], aqdata["particles 10um"], aqdata["particles 25um"], aqdata["particles 50um"], aqdata["particles 100um"]])
 
 file.close()
