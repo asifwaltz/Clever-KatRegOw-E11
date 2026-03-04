dbb905b78606bb6477cac88ea2fb9bd86e0e942b
reggie <88353429+IndigoPlatypus@users.noreply.github.com>
2026-03-04 11:26:45 -0800

    Update Lab3.py
    added the modifications we were meant to do

--- Lab3.py
+++ Lab3.py
@@ -9,11 +9,9 @@ bme680.sea_level_pressure = 1013.25
 
 temperature_offset = -5
 
-while True:
-	print("\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset))
-	print("Gas: %d ohm" % bme680.gas)
-	print("Humidity: %0.1f %%" % bme680.relative_humidity)
-	print("Pressure: %0.3f hPa" % bme680.pressure)
-	print("Altitude = %0.2f meters" % bme680.altitude)
+now = time.time()
+duration = 30
+while (time.time() < now + duration):
+	print("Time is: " + time.time() + "\nTemperature: %0.1f C" % (bme680.temperature + temperature_offset) + "\nGas: %d ohm" % bme680.gas + "\nHumidity: %0.1f %%" % bme680.relative_humidity + "\nPressure: %0.3f hPa" % bme680.pressure + "\nAltitude = %0.2f meters" % bme680.altitude)
 	
 	time.sleep(1)
