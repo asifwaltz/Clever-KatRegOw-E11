c812b19c0106dd56917eb419ebbf62e4c6849ee1
reggie <88353429+IndigoPlatypus@users.noreply.github.com>
2026-04-01 15:40:42 -0700

    Update Lab11.py

--- Lab11.py
+++ Lab11.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     import numpy as np
     import matplotlib.pyplot as plt
 
-    devices = CapeMCA.find_all_mcas()
+    devices = find_all_mcas()
     print(f"Found {len(devices)} MCA device(s)")
 
     if not devices:
